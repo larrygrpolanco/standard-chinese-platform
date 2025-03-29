3e870b15f34b14a185e5dba1d2c8c6cebb40b7da
Larry Grullon-Polanco <larrygrpolanco@gmail.com>
2025-03-29 19:33:03 +0800

    text

--- src/routes/modules/+page.svelte
+++ src/routes/modules/+page.svelte
@@ -150,15 +150,16 @@
 			</div>
 
 			<p class="page-description">
-				Start with the module topic you need/want to work on the most. Figure out your
-				learning style. Take what works for you and leave what doesn't. Learning a language is hard
-				and it isn't linear. Always remember the goal isn't 100% understanding, but to get general message. Go easy on yourself.
+				Start with the module topic you need/want to work on the most. Figure out your learning
+				style. Take what works for you and leave what doesn't. Learning a language is hard and it
+				isn't linear. Always remember the goal isn't 100% understanding, but to get general message.
+				Go easy on yourself.
 			</p>
 			<p class="resource-description">
 				Please note, this resource is not meant to be a standalone learning solution. Instead, it
 				works best when used with Chinese language classes or other practice methods. If Pinyin and
-				Chinese characters are entirely new to you, or if you're seeking a comprehensive
-				refresher,be sure to explore the resource modules in the Guide Page.
+				Chinese characters are entirely new to you, or if you're seeking a tedious refresher, check
+				out the resource modules on the Guide Page.
 			</p>
 		</header>
 
